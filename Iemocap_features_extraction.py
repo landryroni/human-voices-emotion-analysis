c8168f85c727e592eb35921f9eed0455b4eeedd0
landryroni <1197581424@qq.com>
2020-11-01 18:03:58 +0300

    Update Iemocap_features_extraction.py

--- Iemocap_features_extraction.py
+++ Iemocap_features_extraction.py
@@ -97,6 +97,8 @@ def analyse(y,sr,n_fft,hop_length,fmin,fmax):
     return pitches1 #,pitches1,pitches2,pitches3,pitches4,magnitudes
 #####
 import noisereduce as nr
+
+
 # Load audio file
 audio_data, sampling_rate = librosa.load()
 # Noise reduction
@@ -110,6 +112,29 @@ reduced_noise = nr.reduce_noise(audio_clip=audio_data, noise_clip=noisy_part, ve
 # trimmed, index = librosa.effects.trim(reduced_noise, top_db=20, frame_length=512, hop_length=64)
 # print(“Trimmed audio file:”)
 # plotAudio(trimmed)
+
+def get_melspectrogram_db(file_path, sr=None, n_fft=2048, hop_length=512, n_mels=128, fmin=20, fmax=8300, top_db=80):
+  wav,sr = librosa.load(file_path,sr=sr)
+  if wav.shape[0]<5*sr:
+    wav=np.pad(wav,int(np.ceil((5*sr-wav.shape[0])/2)),mode='reflect')
+  else:
+    wav=wav[:5*sr]
+  spec=librosa.feature.melspectrogram(wav, sr=sr, n_fft=n_fft,
+              hop_length=hop_length,n_mels=n_mels,fmin=fmin,fmax=fmax)
+  spec_db=librosa.power_to_db(spec,top_db=top_db)
+
+  return spec_db,sr
+
+
+def spec_to_image(spec, eps=1e-6):
+  mean = spec.mean()
+  std = spec.std()
+  spec_norm = (spec - mean) / (std + eps)
+  spec_min, spec_max = spec_norm.min(), spec_norm.max()
+  spec_scaled = 255 * (spec_norm - spec_min) / (spec_max - spec_min)
+  spec_scaled = spec_scaled.astype(np.uint8)
+  return spec_scaled
+
 def features_extraction(data, sr):
     # mel_extractor = dcase_util.features.MelExtractor()
     # mel = mel_extractor.extract(y= data)
